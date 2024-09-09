08139255ed7a4bcbcab57ff5e0508383b8d412aa
Connor Burnside <163656947+mandeIa@users.noreply.github.com>
2024-09-09 08:55:42 -0500

    Create loader.lua

---
+++ Da Hood/loader.lua
@@ -0,0 +1,2 @@
+script_key = getgenv().hbu.CoreSystems.MainOps.Framework.Authorization.License
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1cd6fa8cfc4c2b12d8ad52bc48f72195.lua"))()
