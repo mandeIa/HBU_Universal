9915e02cecb0be3cbea515b7ffb86ed788e156fc
Connor Burnside <163656947+mandeIa@users.noreply.github.com>
2024-09-09 16:01:59 -0500

    Update Loader.lua

--- Blade Ball/Loader.lua
+++ Blade Ball/Loader.lua
@@ -1,2 +1,2 @@
 script_key = getgenv().hbu.AssistiveTab.MainSystems.Main.License.Key
-loadstring(game:HttpGet("Luarmor"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5d3128d9911bc07af71a16f8dce05fd2.lua"))()
