72cc54f36e5bc012030f0eec350b699942a65736
Connor Burnside <163656947+mandeIa@users.noreply.github.com>
2024-09-09 19:35:49 -0500

    Update Loader.lua

--- Rivals/Loader.lua
+++ Rivals/Loader.lua
@@ -1,2 +1,2 @@
 getgenv().hbu.AssistiveTab.AimSettings.MainSystems.Main.License.Key
-loadstring(game:HttpGet("Luarmor"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d85b0aee13110f78156d3f2e0769f202.lua"))()
